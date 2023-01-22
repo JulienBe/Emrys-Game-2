25b95f150aab11ea3e03663653cee56868f93e3f
JulienBe <bertozzijulien@gmail.com>
2023-01-22 11:26:13 +0100

    Add the ability to display other images for a block

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
 use std::{env, path};
 use ggez::{conf, Context, ContextBuilder, event, GameResult, graphics};
-use ggez::graphics::{Canvas, Color, Image};
+use ggez::graphics::{Canvas, Color, Image as Img};
 
-struct MainState {
-  image1: Image,
+struct Assets {
+  block_imgs: [Img; 2],
+}
+impl Assets {
+  fn new(ctx: &mut Context) -> GameResult<Assets> {
+    Ok(Assets {
+      block_imgs: [
+        Img::from_path(ctx, "/images/block_img1.png")?,
+        Img::from_path(ctx, "/images/block_img2.png")?
+      ]
+    })
+  }
+  fn block_img(&self, block: &Block) -> &Img {
+    &self.block_imgs[block.lvl as usize]
+  }
+}
+struct Block {
+  cell_x: i8,
+  cell_y: i8,
+  lvl: i8,
+}
+impl Block {
+  fn new() -> GameResult<Block> {
+    Ok(Block { cell_x: 0, cell_y: 0, lvl: 1, })
+  }
 }
 
+struct MainState {
+  assets: Assets,
+  block: Block,
+}
 impl MainState {
   fn new(ctx: &mut Context) -> GameResult<MainState> {
-    let s = MainState {
-        image1: Image::from_path(ctx, "/images/image1.png")?,
-    };
-    Ok(s)
+
+    Ok(MainState {
+      assets: Assets::new(ctx)?,
+      block: Block::new().unwrap(),
+    })
   }
 }
 
@@ -21,13 +49,12 @@ impl event::EventHandler<ggez::GameError> for MainState {
   }
   fn draw(&mut self, ctx: &mut Context) -> GameResult {
     let mut canvas = Canvas::from_frame(ctx, Color::from([0.0, 0.0, 0.0, 1.0]));
-    canvas.draw(
-      &self.image1,
-      graphics::DrawParam::new()
-        .dest([50.0, 50.0])
-        .scale([2.0, 2.0])
-        .offset([0.5, 0.5])
-    );
+    let block_img = self.assets.block_img(&self.block);
+    let draw_params = graphics::DrawParam::new()
+      .dest([50.0, 50.0])
+      .scale([2.0, 2.0])
+      .offset([0.5, 0.5]);
+    canvas.draw(block_img, draw_params);
 
     canvas.finish(ctx)?;
     Ok(())
